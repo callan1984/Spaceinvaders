8dbb57a79903da5adc6dc1f30a922518eca2a385
callan1984 <craig@craigallan.uk>
2026-09-05 07:48:42 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+
+class SpaceInvadersApp(App):
+    def build(self):
+        return Label(
+            text="SPACE INVADERS",
+            font_size="32sp"
+        )
+
+
+SpaceInvadersApp().run()
